72602a3cad878c6f647196fc4810adae97abd21a
curryyzheng <curryyzheng@tencent.com>
2020-09-07 21:46:57 +0800

    feat: :sparkles: player, fsm

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayerState;
+public class Player : FSM<Player>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeState(new Idle());
+    }
+}
+
+namespace PlayerState
+{
+    public class Idle : State<Player>
+    {
+        public override IEnumerator Main()
+        {
+            Debug.Log("Hello");
+            while (true)
+            {
+                yield return null;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Utility/FSM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State<T>
+{
+    public T mono;
+    public abstract IEnumerator Main();
+    public virtual void Exit()
+    {
+    }
+    public Coroutine coroutine;
+}
+
+public class FSM<T> : MonoBehaviour where T : FSM<T>
+{
+    public State<T> current = null;
+    public void ChangeState(State<T> next)
+    {
+        if (current != null)
+        {
+            StopCoroutine(current.coroutine);
+            current.Exit();
+        }
+        if (next != null)
+        {
+            next.mono = this as T;
+            next.coroutine = StartCoroutine(next.Main());
+        }
+        current = next;
+    }
+}
