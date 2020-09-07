38a6b69435a12a406a18b421be11790ae0d5d4af
curryyzheng <curryyzheng@tencent.com>
2020-09-07 22:33:04 +0800

    feat: :sparkles: add input system, odin inspector

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using PlayerState;
+using UnityEngine.InputSystem;
+using Sirenix.OdinInspector;
 public class Player : FSM<Player>
 {
     // Start is called before the first frame update
+    public float xAcc;
+    public float xRevAcc;
+    public float xFri;
+    public float xVelMax;
+    [ReadOnly]
+    public Vector2 move = Vector2.zero;
+    [ReadOnly]
+    public Vector2 velocity = Vector2.zero;
     void Start()
     {
         ChangeState(new Idle());
+    }
+    void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>();
     }
 }
 
@@ -20,7 +34,8 @@ namespace PlayerState
             Debug.Log("Hello");
             while (true)
             {
-                yield return null;
+                Debug.Log(mono.move);
+                yield return new WaitForFixedUpdate();
             }
         }
     }
