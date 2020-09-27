e87ce19ba2d9fea15ada3d3662e5d6f6ef63f7f0
curryyzheng <curryyzheng@tencent.com>
2020-09-27 11:25:10 +0800

    feat: Toucher

---
+++ Assets/Scripts/Test/TestToucher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestToucher : Toucher
+{
+    public Text text;
+    private bool isPressed;
+    public override void OnPress()
+    {
+        isPressed = true;
+    }
+
+    public override void OnRelease()
+    {
+        isPressed = false;
+        text.text = "Release" + Position.ToString();
+    }
+    void Update()
+    {
+        if (isPressed)
+        {
+            text.text = "Press" + Position.ToString();
+        }
+    }
+}
