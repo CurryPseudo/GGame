2c42ffa233f6a7d17731155164be89c30d069c4c
curryyzheng <curryyzheng@tencent.com>
2020-09-28 21:23:09 +0800

    feat: dynamic stick

---
+++ Assets/Scripts/Test/TestControlPath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+
+public class TestControlPath : OnScreenControl
+{
+    [InputControl(layout = "Vector2")]
+    [SerializeField]
+    private string m_ControlPath;
+    protected override string controlPathInternal
+    {
+        get => m_ControlPath;
+        set => m_ControlPath = value;
+    }
+}
