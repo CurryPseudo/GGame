814eed281e91c998cfee06cf340ffd9d166649c0
curryyzheng <curryyzheng@tencent.com>
2020-10-04 16:39:08 +0800

    feat: parallax

---
+++ Assets/Scripts/GamePlay/Camera/Parallax.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Vector2 cameraOrigin;
+    private Vector2 origin;
+    public float followRatio;
+    void Start()
+    {
+        cameraOrigin = Camera.main.transform.position;
+        origin = transform.position;
+    }
+    void Update()
+    {
+        transform.position = origin + (Camera.main.transform.position.y - cameraOrigin.y) * followRatio * Vector2.up;
+    }
+}
