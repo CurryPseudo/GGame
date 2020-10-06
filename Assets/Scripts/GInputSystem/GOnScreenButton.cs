afcd47a24d32a9826dc68a35bc64d6a4546eacc9
curryyzheng <curryyzheng@tencent.com>
2020-10-06 14:28:16 +0800

    feat: new player input

---
+++ Assets/Scripts/GInputSystem/GOnScreenButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+
+public class GOnScreenButton : OnScreenControl, IPointerEnterHandler, IPointerExitHandler
+{
+    [InputControl(layout = "Button")]
+    [SerializeField]
+    private string m_ControlPath;
+
+    protected override string controlPathInternal
+    {
+        get => m_ControlPath;
+        set => m_ControlPath = value;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        SendValueToControl(1.0f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        SendValueToControl(0.0f);
+    }
+
+}
