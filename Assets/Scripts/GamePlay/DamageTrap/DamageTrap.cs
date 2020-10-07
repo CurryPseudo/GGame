e88e3a4b9101c5c852f6f01e45e5201456b3cc72
curryyzheng <curryyzheng@tencent.com>
2020-10-07 17:42:39 +0800

    feat: damage trap up down

---
+++ Assets/Scripts/GamePlay/DamageTrap/DamageTrap.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DamageTrapAnimation : MonoBehaviour
+{
+    public abstract void SetRatio(float ratio);
+}
+
+public class DamageTrap : MonoBehaviour
+{
+    public float upTime;
+    public float downTime;
+    public float waitTime;
+    public float holdTime;
+    public float disableBoxRatio;
+    public new DamageTrapAnimation animation;
+    public BoxCollider2D Box
+    {
+        get => box;
+    }
+    private Vector2 originOffset;
+    private Vector2 originSize;
+    private BoxCollider2D box;
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+        originOffset = Box.offset;
+        originSize = Box.size;
+        StartCoroutine(Main());
+    }
+    IEnumerator Main()
+    {
+        while (true)
+        {
+            SetRatio(0);
+            yield return new WaitForSeconds(waitTime);
+            {
+                float timeLeft = upTime;
+                while (timeLeft > 0)
+                {
+                    yield return new WaitForFixedUpdate();
+                    timeLeft -= Time.fixedDeltaTime;
+                    SetRatio(1 - timeLeft / upTime);
+                }
+                SetRatio(1);
+            }
+            yield return new WaitForSeconds(holdTime);
+            {
+                float timeLeft = downTime;
+                while (timeLeft > 0)
+                {
+                    yield return new WaitForFixedUpdate();
+                    timeLeft -= Time.fixedDeltaTime;
+                    SetRatio(timeLeft / upTime);
+                }
+            }
+        }
+    }
+    public void SetRatio(float ratio)
+    {
+        if (animation != null)
+        {
+            animation.SetRatio(ratio);
+        }
+        SetBoxRatio(ratio);
+    }
+    public void SetBoxRatio(float ratio)
+    {
+        if (ratio < disableBoxRatio)
+        {
+            box.enabled = false;
+            return;
+        }
+        box.enabled = true;
+        var offset = originOffset;
+        offset.y = Mathf.Lerp(originOffset.y - originSize.y * 0.5f, originOffset.y, ratio);
+        Box.offset = offset;
+        var size = originSize;
+        size.y = Mathf.Lerp(0, originSize.y, ratio);
+        Box.size = size;
+    }
+}
---
+++ Assets/Scripts/GamePlay/DamageTrap/TestDamageTrapAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDamageTrapAnimation : DamageTrapAnimation
+{
+    private float originScale;
+    private float originY;
+    void Awake()
+    {
+        originY = transform.position.y;
+        originScale = transform.localScale.y;
+    }
+    public override void SetRatio(float ratio)
+    {
+        var position = transform.position;
+        position.y = Mathf.Lerp(originY - originScale * 0.5f, originY, ratio);
+        transform.position = position;
+        var scale = transform.localScale;
+        scale.y = Mathf.Lerp(0, originScale, ratio);
+        transform.localScale = scale;
+    }
+}
