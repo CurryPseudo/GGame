6ba125d0467320ebc7fa8630df2200818534f417
curryyzheng <curryyzheng@tencent.com>
2020-10-10 00:08:35 +0800

    feat: new lock camera

---
+++ Assets/Scripts/Utility/FollowTransform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FollowTransform : MonoBehaviour
+{
+    public Transform followed;
+    public bool lockX;
+    public bool lockY;
+    void Start()
+    {
+        Update();
+    }
+    void Update()
+    {
+        if (followed != null)
+        {
+            var position = transform.position;
+            if (!lockX)
+            {
+                position.x = followed.transform.position.x;
+            }
+            if (!lockY)
+            {
+                position.y = followed.transform.position.y;
+            }
+            transform.position = position;
+        }
+    }
+}
