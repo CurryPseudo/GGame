12f5682fac56c84fb4a4c974d1f8d191c63a9e6a
curryyzheng <curryyzheng@tencent.com>
2020-10-09 18:15:29 +0800

    feat: left right button ui

---
+++ Assets/Scripts/UI/ButtonUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using static UnityEngine.InputSystem.InputAction;
+
+public class ButtonUI : MonoBehaviour
+{
+    public Sprite Pressed;
+    public Sprite Released;
+    public Image Image
+    {
+        get => GetComponent<Image>();
+    }
+    public InputAction action;
+    public void OnEnable()
+    {
+        action.Enable();
+    }
+    public void OnDisable()
+    {
+        action.Disable();
+    }
+    void Awake()
+    {
+        action.performed += Press;
+        action.canceled += Release;
+    }
+    void Press(CallbackContext context)
+    {
+        Image.sprite = Pressed;
+    }
+    void Release(CallbackContext context)
+    {
+        Image.sprite = Released;
+    }
+}
